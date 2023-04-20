f4a157a39d251e1c52442f0b38141def47ddbf15
jingxc <44777925+xingchunjing@users.noreply.github.com>
2023-04-20 14:11:50 +0800

    分页以及导航

--- article/index.js
+++ article/index.js
@@ -9,6 +9,7 @@ const components = [
                 col: "rgb(146, 94, 15)"
             }
         ],
+        attach:['web'],
         desc: "主要介绍一下利用vue完成一个简易的博客网站,记录一些需要注意的问题",
         date: "2019-02-22 23:43:52"
     },
@@ -26,6 +27,7 @@ const components = [
                 col: "red"
             }
         ],
+        attach:['web'],
         desc: "补充:利用github等git搭建可访问的静态资源网站,部署个人网页",
         date: "2023-04-08 19:30:21"
     },
@@ -47,6 +49,7 @@ const components = [
                 col: "rgb(200, 94, 15)"
             }
         ],
+        attach:['web'],
         desc: "主要介绍一下,利用vuepress直接创建一个技术文档的静态页面,无需过多的代码编写",
         date: "2023-04-06 09:30:21"
     },
@@ -68,6 +71,7 @@ const components = [
                 col: "rgb(200, 94, 15)"
             }
         ],
+        attach:['web'],
         desc: "主要记录一下,vue接入gitalk的流程",
         date: "2023-04-07 19:30:21"
     },
@@ -85,6 +89,7 @@ const components = [
                 col: "rgb(146, 68, 123)"
             }
         ],
+        attach:['java'],
         desc: "记录一下自己接入openai所走过的弯路...",
         date: "2023-04-16 13:30:21"
     }

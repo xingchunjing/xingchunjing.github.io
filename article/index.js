6c6b7eec81b584b0d355d0cd181e37415d6df088
jingxc <44777925+xingchunjing@users.noreply.github.com>
2023-04-26 10:10:47 +0800

    google pay

--- article/index.js
+++ article/index.js
@@ -34,6 +34,24 @@ const components = [
         attach:['java'],
         desc: "记录一下自己接入xsolla支付平台",
         date: "2023-04-23 15:49:21"
+    },
+    {
+        id: 'java-google',
+        title: 'google支付服务端验证',
+        path: '/article/java-google.md',
+        type: [
+            {
+                name: 'google',
+                col: "rgb(23, 68, 15)"
+            },
+            {
+                name: 'java',
+                col: "rgb(146, 68, 123)"
+            }
+        ],
+        attach:['java'],
+        desc: "记录一下自己接入google支付，服务端验证的过程",
+        date: "2023-04-25 15:56:21"
     }
 ]
 

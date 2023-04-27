ecef3c1a77bf0bd108896e60c4a0814ac3ffbc22
jingxc <44777925+xingchunjing@users.noreply.github.com>
2023-04-27 19:58:09 +0800

    定位项目问题

--- article/index.js
+++ article/index.js
@@ -52,6 +52,42 @@ const components = [
         attach:['java'],
         desc: "记录一下自己接入google支付，服务端验证的过程",
         date: "2023-04-25 15:56:21"
+    },
+    {
+        id: 'java-google01',
+        title: 'google支付服务端验证',
+        path: '/article/java-google01.md',
+        type: [
+            {
+                name: 'google',
+                col: "rgb(23, 68, 15)"
+            },
+            {
+                name: 'java',
+                col: "rgb(146, 68, 123)"
+            }
+        ],
+        attach:['java'],
+        desc: "google支付服务端验证，补充篇",
+        date: "2023-04-27 16:06:21"
+    },
+    {
+        id: 'linux-jstack',
+        title: 'jstack分析',
+        path: '/article/linux-jstack.md',
+        type: [
+            {
+                name: 'jstack',
+                col: "rgb(23, 68, 15)"
+            },
+            {
+                name: 'linux',
+                col: "rgb(146, 68, 123)"
+            }
+        ],
+        attach:['linux'],
+        desc: "记录一次线上项目内存突然升高的问题查看情况",
+        date: "2023-04-27 16:06:21"
     }
 ]
 
